509612c111912ae846d793da89f076a3672248b4
Anil9436 <dandugulaanil94@gmail.com>
2023-10-11 15:36:06 +0530

    made changes

--- 01_basics/01_intro.cpp
+++ 01_basics/01_intro.cpp
@@ -1,13 +1,34 @@
 #include<iostream>
 
+
+void addTwo(int x, int y)
+{   
+    std::cout<<x<<"+"<<y<<"="<<x+y<<std::endl;
+}
+void subTwo(int x,int y)
+{
+     std::cout<<x<<"-"<<y<<"="<<x-y<<std::endl;   
+}
+
 int main()
 {
-    // [[maybe_unused]]int x{4};
-    // std::cout<<"Hello World\n"<<std::endl;
-    // std::cout<<x<<std::endl;
-    //std::cout<<"Enter the number: ";
-    int x;
-    //std::cin >> x;
-    std::cout<<"Entered value is : "<<x<<std::endl;
+    // // [[maybe_unused]]int x{4};
+    // // std::cout<<"Hello World\n"<<std::endl;
+    // // std::cout<<x<<std::endl;
+    // //std::cout<<"Enter the number: ";
+
+    // std::cout<<"Enter the number you want to be multiplied : ";
+    // int x{};
+    // std::cin>>x;
+    // doNothing(x);
+    // //std::cin >> x;
+    // std::cout<<"Entered value is : "<<alignas<<std::endl;
+    // return 0;
+    // int doubleNum{x*2};
+    // std::cout<<"The value provided is : "<<x<<std::endl;
+    // std::cout<<"The multiplied value is : "<<x*2<<std::endl;
+    int x{6},y{4};
+    addTwo(x,y);
+    subTwo(x,y);
     return 0;
 }
