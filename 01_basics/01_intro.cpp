4167893824f715903b8ecb3d24d9591335fbf76a
Anil9436 <dandugulaanil94@gmail.com>
2023-10-11 14:32:09 +0530

    initial-commit

--- 01_basics/01_intro.cpp
+++ 01_basics/01_intro.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-    std::cout<<"Hello World"<<std::endl;
+    // [[maybe_unused]]int x{4};
+    // std::cout<<"Hello World\n"<<std::endl;
+    // std::cout<<x<<std::endl;
+    //std::cout<<"Enter the number: ";
+    int x;
+    //std::cin >> x;
+    std::cout<<"Entered value is : "<<x<<std::endl;
     return 0;
 }
