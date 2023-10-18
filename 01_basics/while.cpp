a2567ca9344298c74cf1452dd4543d79ef2c0e4d
Anil9436 <86486716+Anil9436@users.noreply.github.com>
2023-10-18 17:01:36 +0000

    Added some more files

---
+++ 01_basics/while.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+    // selection must be declared outside of the do-while-loop, so we can use it later
+    // int selection{};
+
+    // do
+    // {
+    //     std::cout << "Please make a selection: \n";
+    //     std::cout << "1) Addition\n";
+    //     std::cout << "2) Subtraction\n";
+    //     std::cout << "3) Multiplication\n";
+    //     std::cout << "4) Division\n";
+    //     std::cin >> selection;
+    // }
+    // while (selection != 1 && selection != 2 &&
+    //     selection != 3 && selection != 4);
+
+    // // do something with selection here
+    // // such as a switch statement
+
+    // std::cout << "You selected option #" << selection << '\n';
+
+    // return 0;
+    for(int i{1};i<3;i++)
+    {
+        std::cout<<i<<std::endl;
+    }
+    for(int x{1}, y{4};x<10;x++,--y)
+    {
+        std::cout<<x<<" "<<y<<std::endl;
+
+    }
+}
